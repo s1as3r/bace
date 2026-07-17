72d4066bec25bf37506c1d5f05d40b00e5a90312
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-07-18 00:07:09 +0530

    strings: add tests

---
+++ test.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+EXE_PREFIX="test_"
+CC="clang"
+CXX="clang++"
+
+BUILD_PATH="${BUILD_PATH:-$(realpath ./build/tests)}"
+TESTS=$(realpath "./tests/")
+INCLUDES=$(realpath "./include/")
+ACUTEST_PATH=$(realpath "./external/acutest/include/")
+
+BACE_LIB="$BUILD_PATH/bace.o"
+BACE_BUILD_SH=$(realpath "./build.sh")
+
+# debug flags
+DEBUG=(
+    "-g"
+)
+
+# compile time defines
+DEFINES=(
+    "-D_DEFAULT_SOURCE"
+)
+
+# linux platform libraries
+LIBS=(
+    "$BACE_LIB"
+)
+
+# compiler flags
+COMP_FLAGS=(
+    "-I$ACUTEST_PATH" "-I$INCLUDES"
+    "-Wall"
+    "-Wextra"
+    "-Wpedantic"
+    "-Werror"
+    "-Wno-initializer-overrides"
+    "-Og"
+    "-std=c11"
+)
+
+build_bace() {
+    BUILD_PATH="$BUILD_PATH" $BACE_BUILD_SH
+}
+
+no_build=false
+no_run=false
+build_bace=false
+for arg in "$@"; do
+  case $arg in
+    --no-build|-B)
+      no_build=true
+      ;;
+    --no-run|-R)
+      no_run=true
+      ;;
+    --build-bace|-b)
+      build_bace=true
+      ;;
+  esac
+done
+
+# create build directory if it doesn't exist
+if [[ ! -d "$BUILD_PATH" ]]; then
+    echo "created build directory"
+    mkdir -p "$BUILD_PATH"
+fi
+
+if [[ ! -f "$BACE_LIB" || $build_bace == "true" ]]; then
+    echo "=== building bace ==="
+    if ! build_bace; then
+        echo "building bace failed";
+        exit 1;
+    fi
+fi
+
+cd "$BUILD_PATH" || exit
+for file in "$TESTS/*.c"; do
+    fname=$(basename $file)
+    exe_name="$EXE_PREFIX${fname%.*}"
+    exe_cmd=("$CC" "${DEFINES[@]}" "${DEBUG[@]}" "${COMP_FLAGS[@]}" \
+    "-o" "$exe_name" "$TESTS/$fname" "${LIBS[@]}")
+
+    exe_cmd_str=$(IFS=' '; echo "${exe_cmd[*]}")
+    echo "===== $exe_name ====="
+    if [[ $no_build == "false" ]]; then
+        echo "$exe_cmd_str"
+        if ! $(eval "$exe_cmd_str"); then
+            echo "failed building $exe_name "
+        fi
+    fi
+    if [[ $no_run == "false" ]]; then
+        echo ""
+        "./$exe_name"
+    fi
+done
+cd ..
---
+++ tests/strings.c
@@ -0,0 +1,200 @@
+#include "bace/strings.h"
+#include "bace/bace.h"
+#include "bace/thread_context.h"
+#include <stdint.h>
+
+void init_state(void) {
+  init_linux_state();
+}
+
+#define TEST_INIT init_state()
+#include "acutest.h"
+
+void test_str8_list_substr(void) {
+  Str8List zero_list = {0};
+
+  Temp scratch = scratch_begin(0, 0);
+  Arena *arena = scratch.arena;
+  {
+    TEST_CASE("join & list substr");
+    Str8List list = {0};
+    str8_list_pushf(arena, &list, "a");
+    str8_list_pushf(arena, &list, "b");
+    str8_list_pushf(arena, &list, "c");
+
+    {
+      Str8 res = str8_list_join(arena, &list, 0);
+      TEST_CHECK_(str8_match(res, s("abc"), 0), "%s matches abc", res.str);
+    }
+
+    {
+      Str8List sub = str8_list_substr(arena, list, 0, 2);
+      Str8 res = str8_list_join(arena, &sub, 0);
+      TEST_CHECK_(str8_match(res, s("ab"), 0), "%s matches ab", res.str);
+    }
+
+    {
+      Str8List sub = str8_list_substr(arena, list, 0, UINT64_MAX);
+      Str8 res = str8_list_join(arena, &sub, 0);
+      TEST_CHECK_(str8_match(res, s("abc"), 0), "%s matches abc", res.str);
+    }
+  }
+
+  {
+    TEST_CASE("list substr 2");
+    Str8List list = {0};
+    str8_list_pushf(arena, &list, "a");
+    str8_list_pushf(arena, &list, "bcd");
+
+    {
+      Str8List sub = str8_list_substr(arena, list, 2, 3);
+      Str8 res = str8_list_join(arena, &sub, 0);
+      TEST_CHECK_(str8_match(res, s("c"), 0), "%s matches c", res.str);
+    }
+
+    {
+      Str8List sub = str8_list_substr(arena, list, 1, 2);
+      Str8 res = str8_list_join(arena, &sub, 0);
+      TEST_CHECK_(str8_match(res, s("b"), 0), "%s matches b", res.str);
+    }
+  }
+
+  {
+    TEST_CASE("list substr 3");
+    Str8List list = {0};
+    str8_list_pushf(arena, &list, "ab");
+    str8_list_pushf(arena, &list, "cd");
+    str8_list_pushf(arena, &list, "ef");
+
+    {
+      Str8List sub = str8_list_substr(arena, list, 1, 5);
+      Str8 res = str8_list_join(arena, &sub, 0);
+      TEST_CHECK_(str8_match(res, s("bcde"), 0), "%s mathces bcde", res.str);
+    }
+  }
+
+  {
+    TEST_CASE("list substr edge cases");
+    Str8List list = {0};
+    str8_list_pushf(arena, &list, "abc");
+
+    Str8List zero = str8_list_substr(arena, list, 0, 0);
+    TEST_CHECK(memcmp(&zero, &zero_list, sizeof(zero)) == 0);
+
+    Str8List out_of_bounds_range =
+        str8_list_substr(arena, list, UINT64_MAX / 2, UINT64_MAX);
+    TEST_CHECK(memcmp(&out_of_bounds_range, &zero_list, sizeof(zero_list)) == 0);
+  }
+  scratch_end(scratch);
+}
+
+void test_str8_match_wildcard(void) {
+  {
+    TEST_CASE("empty strings");
+    TEST_CHECK(str8_match_wildcard(s(""), s(""), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("**"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("?"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("*?"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("?*"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit(""), 0) == false);
+  }
+
+  {
+    TEST_CASE("exact");
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("a"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("A"), 0) == false);
+  }
+
+  {
+    TEST_CASE("?");
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("?"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("?"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("?"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("a?"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("?b"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("??"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("a?c"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("???"), 0) == false);
+  }
+
+  {
+    TEST_CASE("*");
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("a*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*c"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*b*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("a*c"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("b*"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("**"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("a**c"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("a*b*c"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*a*d*"), 0) == false);
+  }
+
+  {
+    TEST_CASE("* hard");
+    TEST_CHECK(str8_match_wildcard(str8_lit("abcd"), str8_lit("a*d"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abefcdgiescdfimde"), str8_lit("ab*cd?i*de"),
+                                   0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("mississippi"), str8_lit("m*iss*ppi"), 0) ==
+               1);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*b"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("aa"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("aa"), str8_lit("a"), 0) == false);
+  }
+
+  {
+    TEST_CASE("case insensitive");
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("A"),
+                                   StringMatchFlag_CaseInsensitive) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("FooBar"), str8_lit("foobar"),
+                                   StringMatchFlag_CaseInsensitive) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("Foobar"), str8_lit("foo*"),
+                                   StringMatchFlag_CaseInsensitive) == true);
+  }
+
+  {
+    TEST_CASE("right side sloppy");
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("ab"),
+                                   StringMatchFlag_RightSideSloppy) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit(""),
+                                   StringMatchFlag_RightSideSloppy) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("a"),
+                                   StringMatchFlag_RightSideSloppy) == false);
+  }
+
+  {
+
+    TEST_CASE("slash insensitive");
+    TEST_CHECK(str8_match_wildcard(str8_lit("a/b"), str8_lit("a\\b"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a/b"), str8_lit("a\\b"),
+                                   StringMatchFlag_SlashInsensitive) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a/b/c"), str8_lit("a\\*\\c"),
+                                   StringMatchFlag_SlashInsensitive) == true);
+  }
+
+  {
+    TEST_CASE("combined");
+    TEST_CHECK(str8_match_wildcard(str8_lit("Ab\\Cde"), str8_lit("ab/*e"),
+                                   StringMatchFlag_CaseInsensitive |
+                                       StringMatchFlag_SlashInsensitive) == true);
+
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("*?*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("*?*"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit(""), str8_lit("*?*"), 0) == false);
+    TEST_CHECK(str8_match_wildcard(str8_lit("abc"), str8_lit("?*?"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("ab"), str8_lit("?*?"), 0) == true);
+    TEST_CHECK(str8_match_wildcard(str8_lit("a"), str8_lit("?*?"), 0) == false);
+  }
+}
+
+// clang-format off
+TEST_LIST = {
+  {"str8_list_substr", test_str8_list_substr},
+  {"str8_match_wildcard", test_str8_match_wildcard},
+  {NULL, NULL}
+};
+// clang-format on
